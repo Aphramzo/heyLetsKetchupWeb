536a6cc836b8cc710c81212a4851015b20649710
Tyler Walborn <im.t.wal@gmail.com>
2019-03-10 21:27:09 -0600

    Correctly passing creds through login

--- src/api/authentication.api.js
+++ src/api/authentication.api.js
@@ -2,9 +2,7 @@ import axios from 'axios';
 
 const login = (creds) => {
   console.log('url is', process.env.API_URL);
-  axios.post(`${process.env.API_URL}/login`, {
-    data: creds,
-  });
+  axios.post(`${process.env.API_URL}/login`, creds);
 };
 
 export default login;
